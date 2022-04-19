3af335c3243de09ecff86d5b0da36200bddba339
Keyla Mariela Carrera Prieto <marcarrera00mail.com>
2022-04-19 18:32:59 -0500

    servicios by Mar Carrera

--- src/views/DetailsService.jsx
+++ src/views/DetailsService.jsx
@@ -3,16 +3,9 @@ import React from 'react';
 function DetailsService(){
     return(
         <div>
-            
-           
-            
             <div className ='name'>
                 <h1><span>Detalles del Servicio</span></h1>
-
             </div>
-            
-            
-
         </div>
     )
 }
